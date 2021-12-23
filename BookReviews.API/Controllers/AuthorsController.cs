f07d4579ecc3bd43abf4151ae678dbaa09d6f521
Ervis Trupja <ervistrupja@outlook.com>
2021-12-23 10:56:30 +0100

    23 dhjetor 2021

--- BookReviews.API/Controllers/AuthorsController.cs
+++ BookReviews.API/Controllers/AuthorsController.cs
@@ -1,4 +1,6 @@
-﻿using BookReviews.API.Data.Sample;
+﻿using BookReviews.API.Data;
+using BookReviews.API.Data.DTOs;
+using BookReviews.API.Data.Sample;
 using BookReviews.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,34 +15,71 @@ namespace BookReviews.API.Controllers
     [ApiController]
     public class AuthorsController : ControllerBase
     {
+        private AppDbContext _context;
+        public AuthorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult GetAllAuthors()
         {
-            return Ok();
+            var result = _context.Authors.ToList();
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetAuthorById(int id)
         {
-            return Ok();
+            var result = _context.Authors.FirstOrDefault(n => n.Id == id);
+            return Ok(result);
         }
 
         [HttpPost]
-        public IActionResult AddAuthor()
+        public IActionResult AddAuthor([FromBody]AuthorDTO author)
         {
+            var _author = new Author()
+            {
+                FirstName = author.FirstName,
+                LastName = author.LastName,
+                DateCreated = DateTime.Now
+            };
+
+            _context.Authors.Add(_author);
+            _context.SaveChanges();
+
             return Ok();
         }
 
-        [HttpPut]
-        public IActionResult UpdateAuthor()
+        [HttpPut("{id}")]
+        public IActionResult UpdateAuthor(int id, [FromBody]AuthorDTO author)
         {
-            return Ok();
+            var dbAuthor = _context.Authors.FirstOrDefault(x => x.Id == id);
+            if (dbAuthor == null)
+            {
+                return NotFound();
+            } else
+            {
+                dbAuthor.FirstName = author.FirstName;
+                dbAuthor.LastName = author.LastName;
+                _context.SaveChanges();
+                return Ok("Author updated");
+            }
         }
 
-        [HttpDelete]
-        public IActionResult DeleteAuthor()
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(int id)
         {
-            return Ok();
+            var dbAuthor = _context.Authors.FirstOrDefault(x => x.Id == id);
+            if(dbAuthor == null)
+            {
+                return NotFound($"Author with id = {id} not found");
+            } else
+            {
+                _context.Authors.Remove(dbAuthor);
+                _context.SaveChanges();
+                return Ok($"Autori me id = {id} u fshi nga databaza");
+            }
         }
     }
 }
---
+++ BookReviews.API/Data/DTOs/AuthorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReviews.API.Data.DTOs
+{
+    public class AuthorDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
